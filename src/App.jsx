1f7057b05a67df3b596e12b3c57afd86924acf5d
Vijay Singh <thakurvijayofficial@gmail.com>
2025-04-29 19:50:33 +0530

    Init App

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return <Navbar />;
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-light">
+      <div className="container">
+        <div className="py-2">
+          <h5 className="m-0">GU iClouds EMS</h5>
+        </div>
+      </div>
+    </div>
+  );
+}
