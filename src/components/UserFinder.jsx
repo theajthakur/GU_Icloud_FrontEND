1ae9667645e57ba48b2f370a7d19fc8c3ee7014b
Vijay Singh <thakurvijayofficial@gmail.com>
2025-04-30 16:37:01 +0530

    Showing detailed sample Data

--- src/components/UserFinder.jsx
+++ src/components/UserFinder.jsx
@@ -5,8 +5,10 @@ export default function UserFinder() {
   const [students, setStudents] = useState(null);
   const [search, setSearch] = useState("");
   const [admission, setAdmission] = useState(null);
+  const [password, setPassword] = useState("");
+  const [data, setData] = useState(null);
   useEffect(() => {
-    if (!search || search.length.trim() < 5) return setStudents(null);
+    if (!search || search.trim().length < 5) return setStudents(null);
     let filtered;
     filtered = studentsData.filter((aa) =>
       aa.name.trim().toLowerCase().includes(search.trim())
@@ -17,6 +19,11 @@ export default function UserFinder() {
     setStudents(filtered);
   }, [search]);
 
+  const handleUserSearch = () => {
+    if (!admission?.adm_no || !password)
+      return alert("Please enter all the details");
+    setData(admission);
+  };
   return (
     <div className="container mt-4 overflow-hidden">
       <div className="bg-light p-3">
@@ -27,70 +34,166 @@ export default function UserFinder() {
                 <h3 className="text-center search-heading">
                   <strong>Search Student</strong>
                 </h3>
-                <div className="main-search">
-                  <div className="input-container">
-                    <input
-                      autoCorrect="off"
-                      autoComplete="off"
-                      autoCapitalize="off"
-                      spellCheck="false"
-                      className="form-control"
-                      placeholder="Search Student"
-                      value={search}
-                      onChange={(e) => {
-                        setSearch(e.target.value.toLowerCase());
+                {admission ? (
+                  <div className="password-container">
+                    <a
+                      href="#"
+                      onClick={() => {
+                        setAdmission(null);
+                        setData(null);
+                        setPassword("");
                       }}
-                    />
+                    >
+                      Back
+                    </a>
+                    <div className="py-2">
+                      <p className="m-0">
+                        <b>Name: </b>
+                        <span className="ms-2">{admission.name}</span>
+                      </p>
+                      <p>
+                        <b>Admission Number: </b>
+                        <span className="ms-2">{admission.adm_no}</span>
+                      </p>
+                    </div>
+                    <div className="d-inline-flex w-100">
+                      <div className="w-100">
+                        <input
+                          className="form-control rounded-0"
+                          placeholder={`Enter Mobile Number of ${admission.name}`}
+                          value={password}
+                          onChange={(e) => {
+                            setPassword(e.target.value);
+                          }}
+                          autoCorrect="off"
+                          autoComplete="off"
+                          autoCapitalize="off"
+                          spellCheck="false"
+                        />
+                      </div>
+                      {password ? (
+                        <div>
+                          <button
+                            className="btn btn-primary rounded-0"
+                            onClick={handleUserSearch}
+                          >
+                            Search
+                          </button>
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                    </div>
                   </div>
-                  {students ? (
-                    <div className="suggestion-container">
-                      <div className="suggestion-box">
-                        {students.map((student, index) => (
-                          <div key={index}>
-                            {index == 0 ? (
-                              ""
-                            ) : (
-                              <div className="border-top"></div>
-                            )}
-                            <div className="suggestion-unit p-2">
-                              <div id="student-name">
-                                <p className="m-0">{student.name}</p>
-                              </div>
-                              <div id="father-name">
-                                <p className="m-0 small text-secondary">
-                                  {student.father}
-                                </p>
-                              </div>
-                              <div id="admission-number">
-                                <p className="m-0 small text-secondary">
-                                  {student.adm_no}
-                                </p>
+                ) : (
+                  <div className="main-search">
+                    <div className="input-container">
+                      <input
+                        autoCorrect="off"
+                        autoComplete="off"
+                        autoCapitalize="off"
+                        spellCheck="false"
+                        className="form-control"
+                        placeholder="Search Student"
+                        value={search}
+                        onChange={(e) => {
+                          setSearch(e.target.value.toLowerCase());
+                        }}
+                      />
+                    </div>
+                    {students ? (
+                      <div className="suggestion-container">
+                        <div className="suggestion-box">
+                          {students.map((student, index) => (
+                            <div key={index}>
+                              {index == 0 ? (
+                                ""
+                              ) : (
+                                <div className="border-top"></div>
+                              )}
+                              <div className="suggestion-unit p-2">
+                                <div id="student-name">
+                                  <p
+                                    className="m-0"
+                                    onClick={() => {
+                                      setAdmission(student);
+                                    }}
+                                  >
+                                    {student.name}
+                                  </p>
+                                </div>
+                                <div id="father-name">
+                                  <p className="m-0 small text-secondary">
+                                    {student.father}
+                                  </p>
+                                </div>
+                                <div id="admission-number">
+                                  <p className="m-0 small text-secondary">
+                                    {student.adm_no}
+                                  </p>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        ))}
+                          ))}
+                        </div>
                       </div>
-                    </div>
-                  ) : (
-                    ""
-                  )}
-                </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                )}
               </div>
             </div>
           </div>
           <div className="col-md-6">
             <div className="h-100 sidebar-show-beautify animate__animated animate__fadeInRight animate__delay-1s">
               <div className="d-inline-flex w-100 h-100 justify-content-center align-items-center">
-                <div className="text-center heading-big my-4">
-                  <h1 className="text-secondary">
-                    <strong>Student Finder</strong>
-                  </h1>
-                  <p className="text-secondary animate__animated animate__fadeInUp animate__delay-2s">
-                    <strong>
-                      Search for a student by name or admission number
-                    </strong>
-                  </p>
-                </div>
+                {data ? (
+                  <div className="student-info">
+                    <table className="table table-bordered">
+                      <tbody>
+                        <tr>
+                          <td>
+                            <strong>Student's Name</strong>
+                          </td>
+                          <td>{data.name}</td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <strong>Father's Name</strong>
+                          </td>
+                          <td>{data.father}</td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <strong>Admission Number</strong>
+                          </td>
+                          <td>{data.adm_no}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div className="mt-2">
+                      <p className="m-0 lead">
+                        <b className="text-primary">Maintenance: </b>
+                        <span>
+                          Detailed data will be shown to you after some changes
+                          !
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="text-center heading-big my-4">
+                    <h1 className="text-secondary">
+                      <strong>Student Finder</strong>
+                    </h1>
+                    <p className="text-secondary animate__animated animate__fadeInUp animate__delay-2s">
+                      <strong>
+                        Search for a student by name or admission number
+                      </strong>
+                    </p>
+                  </div>
+                )}
               </div>
             </div>
           </div>
